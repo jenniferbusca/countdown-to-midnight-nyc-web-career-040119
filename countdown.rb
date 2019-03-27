490d9b0acb51aad160eae88f8a5a25d6f58a9505
Jennifer Busca <jennifer.busca@gmail.com>
2019-03-27 19:49:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,12 +9,12 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-def countdown(number)
+def countdown_with_sleep(number)
   counter = number
   while counter > 0
     puts "#{counter} SECOND(S)!"
     counter -= 1
+    sleep(1)
   end
   return "HAPPY NEW YEAR!"
 end
-sleep(num_secs)
